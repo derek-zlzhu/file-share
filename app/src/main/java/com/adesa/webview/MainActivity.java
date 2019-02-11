a4d4563268dce008f803be7e32a329826bc74fad
Zhu, Derek <Zhaolong.Zhu@openlane.com>
2019-02-11 14:42:24 -0500

    updates for the camera permission request

--- app/src/main/java/com/adesa/webview/MainActivity.java
+++ app/src/main/java/com/adesa/webview/MainActivity.java
@@ -1,13 +1,14 @@
 package com.adesa.webview;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.webkit.ConsoleMessage;
 import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -17,6 +18,38 @@ public class MainActivity extends Activity {
 
     private WebView webView = null;
     public static final String TAG = MainActivity.class.getSimpleName();
+    private final int CAMERA_PERMISSION_REQUEST = 0;
+    private boolean mDeniedCameraAccess = false;
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void grantCameraPermissionsThenStartScanning() {
+        if (this.checkSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (!mDeniedCameraAccess) {
+                // It's pretty clear for why the camera is required. We don't need to give a
+                // detailed reason.
+                this.requestPermissions(new String[]{ Manifest.permission.CAMERA },
+                        CAMERA_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mDeniedCameraAccess = false;
+            } else {
+                mDeniedCameraAccess = true;
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,40 +85,23 @@ public class MainActivity extends Activity {
         WebViewClientImpl webViewClient = new WebViewClientImpl(this);
         webView.setWebViewClient(webViewClient);
 
-        webView.setWebChromeClient(new WebChromeClient()
-        {
-            @Override
-            public boolean onConsoleMessage(ConsoleMessage consoleMessage)
-            {
-                Log.d(TAG, "WebChromeClient consoleMessage: " + consoleMessage.message());
-
-                // if "Uncaught SyntaxError: Unexpected end of input" error message is caught
-                // we must load the webview using loadUrl instead of loadDataWithBaseURL
-                // this is due to a bug with webview.loadDataWithBaseURL can't process JS with comments
-
-                return true;
-            }
+        webView.setWebChromeClient(new WebChromeClient() {
 
             @Override
-            public void onPermissionRequest(final PermissionRequest request)
-            {
-//                Log.d(TAG, "onPermissionRequest");
-//					request.grant(request.getResources());
-
-//					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//						request.grant(request.getResources());
-//					}
-
+            public void onPermissionRequest(final PermissionRequest request) {
+                Log.d(TAG, "onPermissionRequest");
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
                     @Override
                     public void run() {
-                        Log.i(TAG, "|> onPermissionRequest run");
+                        grantCameraPermissionsThenStartScanning();
+
                         request.grant(request.getResources());
+
                     }
                 });
-
             }
+
         });
 
         webView.loadUrl("https://s3.amazonaws.com/olm-web-dev/gens/scan/scan-into-input/index.html");
